25772761471a7eec346d7889c98eb931bd068008
Vi-vi-1 <tarasovavika204@gmail.com>
2022-10-07 09:46:12 +0300

    init

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,73 @@
+fun main() {
+
+//Первое задание
+    val exercises : Int = 13;
+    var exercisesSolved : Int = 0;
+    exercisesSolved +=1;
+    println(exercises);
+    println(exercisesSolved);
+
+// Второе задание
+    val Age : Int = 18;
+    println(Age);
+    exercisesSolved += 1;
+
+// Третье задание
+    var agemiddle : Double = 18.0;
+    agemiddle = (30.0 + 18.0) / 2;
+    println(agemiddle);
+    exercisesSolved += 1;
+
+ // Четвертое задание
+    val testNumber : Double = 25.0;
+    val evenOdd : Double = testNumber%2;
+    println(testNumber);
+    println(evenOdd);
+    exercisesSolved += 1;
+
+//Пятое задание
+    var answer : Int = 0;
+    answer = answer + 1 ;
+    answer = answer + 10;
+    answer = answer * 10;
+    answer = answer shr(3);
+    println(answer);
+    exercisesSolved += 1;
+
+// Шестое задание
+    var age : Int = 16;
+    print (age)
+    age = 30;
+    print (age)
+    exercisesSolved += 1;
+
+ // Седьмое задание
+    val a : Int = 46;
+    val b : Int = 10;
+
+    val answer1: Int = (a*100) + b;
+    println (answer1);
+    val answer2: Int = (a*100) + (b*100);
+    println (answer2);
+    val answer3: Int = (a*100) + (b/10);
+    println (answer3);
+    exercisesSolved += 1;
+
+ // Восьмое задание
+    5*3-4/2*2
+
+ // Девятое задание
+    val a1 : Double = 34.0;
+    val b1 : Double = 17.0;
+    val awerage : Double = (a1 + b1)/2;
+    println (awerage);
+    exercisesSolved += 1;
+
+//Десятое задание
+    val farenheit : Double = 460.0;
+    val celcius : Double = (farenheit-32.0)/1.8;
+    println (celcius);
+    exercisesSolved += 1;
+
+
+}
